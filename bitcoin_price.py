8dc1c46a53930dae1e4e409e109facac75b39c92
inned454 <inned454@gmail.com>
2026-01-19 16:40:01 +0700

    Update bitcoin_price.py

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,16 +1,17 @@
-import requset
+import requests
 
 def get_bitcoin_price():
     # เราจะดึงข้อมูลราคา Bitcoin จาก Public API
     url = "https://api.bitkub.com/api/market/ticker?sym=THB_BTC"
     response = requests.get(url)
 
-    if respones.status_code == 200:
+    if response.status_code == 200:
         data = response.json()
         bitcoin_price = data['THB_BTC']['last']
         return bitcoin_price
     else:
         print("Error fetching data from API")
-        
+
 if __name__ == "__main__":
-    get_bitcoin_price()
+    print(get_bitcoin_price())
+
